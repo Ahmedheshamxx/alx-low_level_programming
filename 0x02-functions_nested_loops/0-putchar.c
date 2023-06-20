f59801ca2781ed61963b2112bfd897461c5c59ed
Yoe <you@example.com>
2023-06-20 16:41:50 -0700

    a

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "mani.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: print aplhabet in lowercase then in uppercase
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch + 0; ch < 8; ch++)
+		_putchar(str[ch]);
+	_putchar('\n');
+
+	return (0);
+}
