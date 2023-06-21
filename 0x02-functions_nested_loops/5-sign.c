151ded706ce58f0571d0491e9b8936497001b49f
Yoe <you@example.com>
2023-06-20 18:31:02 -0700

    a

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,12 +2,12 @@
 
 /**
  * print_sign - print + if n is greater than zero,
- *              0 if n is zero and - if n is less
- *              than zero.
+ *             0 if n is zero and - if n is less
+ *             than zero.
  *
- *n: takes integer type input for function.
+ * @n: takes integer type input for function.
  *
- * Return: returns 1 if +, 0 if 0 and -1 if -
+ * Return: 1 if +, 0 if 0 and -1 if -
 */
 
 int print_sign(int n)
@@ -28,3 +28,4 @@ int print_sign(int n)
 		return (-1);
 	}
 }
+
